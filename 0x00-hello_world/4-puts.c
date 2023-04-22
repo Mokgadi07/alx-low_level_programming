edcf605421b1fc714c5cb0a6436965210ad6524c
Mokgadi07 <mokgady07@gmail.com>
2023-04-21 20:30:33 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-echo "Programming is like building a multilingual puzzle"
+#include <stdio.h> // include standard input/output library
+
+int main() { // define main function
+    puts("Programming is like building a multilingual puzzle"); // use puts function to print the given string
+    return 0; // return 0 to indicate successful program termination
+}
