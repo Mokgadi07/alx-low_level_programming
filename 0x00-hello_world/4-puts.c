3868fddf9ab5378b4754e89c32e85e2874fd68b1
Mokgadi07 <mokgady07@gmail.com>
2023-04-24 02:10:19 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
-/*
- * This program prints the message "Programming is like building a multilingual puzzle"
+/**
+ * main - This programme prints the message
+ * "Programming is like building a multilingual puzzle"
  * using the `puts()` function.
+ * return (0);
  */
 int main(void)
 {
-    /* Call the `puts()` function to print the message to the console */
-    puts("Programming is like building a multilingual puzzle");
+/* Call the `puts()` function to print the message to the console */
+puts("Programming is like building a multilingual puzzle");
 
-    return (0);
+return (0);
 }
