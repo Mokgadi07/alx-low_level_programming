d4fe79eea1f8fad89cd07ad8e46e5429615177b1
Mokgadi07 <mokgady07@gmail.com>
2023-04-24 02:14:07 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 /* Call the `puts()` function to print the message to the console */
-puts("Programming is like building a multilingual puzzle");
-
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
